66b9d9e99803c07ec21feabf175f74de59a117fd
Vitaly Shvetsov <vitaly.shvetsov@sharp-dev.net>
2020-02-27 20:43:33 +0500

    10.121

--- UdemyAngular/src/app/recipes/recipe.service.ts
+++ UdemyAngular/src/app/recipes/recipe.service.ts
@@ -1,18 +1,27 @@
 import { Recipe } from './recipe-list/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
-            'a test recipe 1', 
-            'Some description 1', 
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/7-up_Logo.svg/2000px-7-up_Logo.svg.png'),
-        new Recipe(
-            'a test recipe 2', 
-            'Some description 2', 
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/7-up_Logo.svg/2000px-7-up_Logo.svg.png')];
+          'Tasty Schnitzel',
+          'A super-tasty Schnitzel - just awesome!',
+          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+          ]),
+        new Recipe('Big Fat Burger',
+          'What else you need to say?',
+          'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+          ])
+      ];
     
     getRecipes() {
         return this.recipes.slice();

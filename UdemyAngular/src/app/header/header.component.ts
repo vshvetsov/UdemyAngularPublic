724620b323993e1b2de8d50cd033ab44200ad76f
Vitaly Shvetsov <vitaly.shvetsov@sharp-dev.net>
2020-03-03 14:33:32 +0500

    156 & 157

--- UdemyAngular/src/app/header/header.component.ts
+++ UdemyAngular/src/app/header/header.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();  
-
-  onSelected(feature: string) {    
-    this.featureSelected.emit(feature);
-  }
+export class HeaderComponent {  
 }
